464931c457783382e6c27fab54e4c00b87037d07
Sebastian Villa <sebastianv524@gmail.com>
2019-09-12 10:00:07 -0500

    Edicion de NavBar
    Falta limitar la barra de navegacion unicamente cuando se loguee un usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent as LoginComponent } from './modules/login/components/layout/layout.component';
-import { LayoutComponent as DashBoardComponent } from './modules/dashboard/components/layout/layout.component';
-import { LayoutComponent as CalendarioComponent } from './modules/calendario/components/layout/layout.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent as LoginComponent } from "./modules/login/components/layout/layout.component";
+import { LayoutComponent as DashBoardComponent } from "./modules/dashboard/components/layout/layout.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashBoardComponent },
-  { path: 'calendario', component: CalendarioComponent }
+  { path: "", pathMatch: "full", redirectTo: "login" },
+
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashBoardComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { ApolloModule, Apollo } from "apollo-angular";
 import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginModule } from './modules/login/login.module';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
-import { CalendarioModule } from './modules/calendario/calendario.module';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginModule } from "./modules/login/login.module";
+import { DashboardModule } from "./modules/dashboard/dashboard.module";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,37 +22,23 @@ import { CalendarioModule } from './modules/calendario/calendario.module';
     HttpClientModule,
     FormsModule,
     LoginModule,
-    DashboardModule,
-    CalendarioModule
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
-  constructor(
-    private apollo: Apollo,
-    private httpLink: HttpLink
-  ) {
-
-    apollo.create(
-      {
-        link: httpLink.create(
-          {
-            uri: 'http://35.192.7.211:8000/graphql'
-          }
-        ),
-        cache: new InMemoryCache()
-      }
-    );
-
+  constructor(private apollo: Apollo, private httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({
+        uri: "http://35.192.7.211:8000/graphql"
+      }),
+      cache: new InMemoryCache()
+    });
   }
-
-
-
 }
 
-//npm i 
+//npm i
 //npm i --save apollo-angular apollo-client graphql-tag
 //npm i --save apollo-angular-link-http
 //npm i --save graphql
