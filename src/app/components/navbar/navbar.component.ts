189a6948a20271a2141085e5d109b4931f8a0ce2
Sebastian Villa <sebastianv524@gmail.com>
2019-09-14 20:48:38 -0500

    interfaz funcional

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit, Host } from '@angular/core';
-import { LoginService } from '../../modules/login/services/login.service';
-import { AppComponent } from 'src/app/app.component';
-import { UserWeb } from '../../models/UserWeb';
+import { Component, OnInit, Host } from "@angular/core";
+import { LoginService } from "../../modules/login/services/login.service";
+import { AppComponent } from "src/app/app.component";
+import { UserWeb } from "../../models/UserWeb";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent implements OnInit {
-
-  public user: UserWeb
+  public user: UserWeb;
 
   constructor(
     @Host() public app: AppComponent,
     private loginSrv: LoginService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.user = this.loginSrv.getCurrentUser()
+    this.user = this.loginSrv.getCurrentUser();
   }
-
 }
--- src/app/modules/dashboard/components/layout/layout.component.ts
+++ src/app/modules/dashboard/components/layout/layout.component.ts
@@ -1,38 +1,34 @@
-import { Component, OnInit, Host } from '@angular/core';
-import { LoginService } from '../../../login/services/login.service';
-import { UserWeb } from '../../../../models/UserWeb';
-import { Router } from '@angular/router';
-import { AppComponent } from 'src/app/app.component';
+import { Component, OnInit, Host } from "@angular/core";
+import { LoginService } from "../../../login/services/login.service";
+import { UserWeb } from "../../../../models/UserWeb";
+import { Router } from "@angular/router";
+import { AppComponent } from "src/app/app.component";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./layout.component.html"
 })
 export class LayoutComponent implements OnInit {
-
   public user: UserWeb;
 
   constructor(
-
     private loginSrv: LoginService,
     private router: Router,
     @Host() private app: AppComponent
-  ) { }
+  ) {}
 
   async ngOnInit() {
-    this.user = this.loginSrv.getCurrentUser()
-    this.app.showNav = true
+    this.user = this.loginSrv.getCurrentUser();
+    this.app.showNav = true;
     console.log(this.user);
-
   }
 
   btnCerrarSesion() {
-    this.loginSrv.LOGOUT()
-    this.router.navigate(['login'])
+    this.loginSrv.LOGOUT();
+    this.router.navigate(["login"]);
   }
   btnCalendario() {
-    this.router.navigate(['calendario'])
+    this.router.navigate(["calendario"]);
   }
 }
 
